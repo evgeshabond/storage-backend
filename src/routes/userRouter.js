ebff8779089a857b3e6736020b5e82f926ebdfa2
Eugen Bondarenko <eugeniy_geniy@mail.ru>
2020-11-24 17:21:35 +0100

    created api for admin init

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,153 +1,168 @@
-const express = require('express')
-const User = require ('../models/userModel')
+const express = require("express");
+const User = require("../models/userModel");
 const bcryptjs = require("bcryptjs");
 
-const auth = require('../middleware/auth')
+const auth = require("../middleware/auth");
 
-const router = express.Router()
+const router = express.Router();
+
+//Create admin
+router.post("/users/createadmin", async (req, res) => {
+  const user = new User({
+    login: "admin",
+    password: "admin",
+    firstName: "admin",
+    lastName: "admin",
+  });
+  try {
+    await user.save();
+    const token = await user.createAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
 
 // Create user
-router.post('/users',auth ,async (req, res) => {
-    if (!req.user || req.user.login != 'admin') {
-        console.log('you are not admin')
-        return res.status(403).send('Access denied')
-
-    }
-    const user = new User(req.body)
-    try {
-        await user.save()
-        const token = await user.createAuthToken()
-        res.send({user, token})
-    }
-    catch (e) {
-        res.status(400).send(e.message)
-    }
-})
-
-
+router.post("/users", auth, async (req, res) => {
+  if (!req.user || req.user.login != "admin") {
+    console.log("you are not admin");
+    return res.status(403).send("Access denied");
+  }
+  const user = new User(req.body);
+  try {
+    await user.save();
+    const token = await user.createAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
 
 //Update user
-router.patch('/users/me', auth, async(req, res) => {
-    const updatesList = Object.keys(req.body)
-    const allowedUpdatesList = ['login','password']
+router.patch("/users/me", auth, async (req, res) => {
+  const updatesList = Object.keys(req.body);
+  const allowedUpdatesList = ["login", "password"];
 
-    const isValid = updatesList.every((update) => allowedUpdatesList.includes(update))
+  const isValid = updatesList.every((update) =>
+    allowedUpdatesList.includes(update)
+  );
 
-    if (!isValid) {
-        return res.status(400).send('Invalid request')
-    }
+  if (!isValid) {
+    return res.status(400).send("Invalid request");
+  }
 
-    try {
-        updatesList.forEach((update) => {
-            req.user[update] = req.body[update]
-        })
-        await req.user.save()
-        res.send('User modified')
-    } catch(e) {
-        res.status(500).send('User is not modified')
-    }
-
-})
+  try {
+    updatesList.forEach((update) => {
+      req.user[update] = req.body[update];
+    });
+    await req.user.save();
+    res.send("User modified");
+  } catch (e) {
+    res.status(500).send("User is not modified");
+  }
+});
 
 //Update user from admin by id
-router.patch('/users/:id', auth, async(req, res) => {
+router.patch("/users/:id", auth, async (req, res) => {
+  if (!req.user || req.user.login != "admin") {
+    console.log("you are not admin");
+    return res.status(403).send("Access denied");
+  }
+  const _id = req.params.id;
 
-    if (!req.user || req.user.login != 'admin') {
-        console.log('you are not admin')
-        return res.status(403).send('Access denied')
+  const updatesList = Object.keys(req.body);
+  const allowedUpdatesList = ["login", "password", "firstName", "lastName"];
 
-    }
-    const _id = req.params.id;
-    
-    const updatesList = Object.keys(req.body)
-    const allowedUpdatesList = ['login','password','firstName','lastName' ]
+  const isValid = updatesList.every((update) =>
+    allowedUpdatesList.includes(update)
+  );
 
-    const isValid = updatesList.every((update) => allowedUpdatesList.includes(update))
+  if (!isValid) {
+    return res.status(400).send("Invalid request");
+  }
 
-    if (!isValid) {
-        return res.status(400).send('Invalid request')
-    }
-
-    try {
-        const user = await User.findById(_id)
-        if (!user) return res.status(404).send('User not found')
-        updatesList.forEach((update) => {
-            user[update] = req.body[update]
-        })
-        await user.save()
-        res.send('User modified')
-    } catch(e) {
-        res.status(500).send('User is not modified')
-    }
-
-})
+  try {
+    const user = await User.findById(_id);
+    if (!user) return res.status(404).send("User not found");
+    updatesList.forEach((update) => {
+      user[update] = req.body[update];
+    });
+    await user.save();
+    res.send("User modified");
+  } catch (e) {
+    res.status(500).send("User is not modified");
+  }
+});
 
 //User login
-router.post('/users/login', async (req, res) => {
-    try{
-        // console.log(req.body)
-        const user = await User.findByCredentials(req.body.login, req.body.password)
-        const token = await user.createAuthToken()
-        res.status(200).send({user, token})
-    } catch(e) {
-        res.status(404).send('Failed to log in')
-    }
-})
+router.post("/users/login", async (req, res) => {
+  try {
+    // console.log(req.body)
+    const user = await User.findByCredentials(
+      req.body.login,
+      req.body.password
+    );
+    const token = await user.createAuthToken();
+    res.status(200).send({ user, token });
+  } catch (e) {
+    res.status(404).send("Failed to log in");
+  }
+});
 
-//Get all users 
+//Get all users
 // ?changesLimit
-router.get('/users', auth, async (req, res) => {
-    if (!req.user || req.user.login != 'admin') {
-        console.log('you are not admin')
-        return res.status(403).send('Access denied')
-
-    }
-    try {
-        const users = await User.find({}).
-            populate('articles').
-            populate({
-                path: 'changes',
-                options: {
-                    limit: parseInt(req.query.changesLimit)
-                }
-            })
-        users.forEach((user) => user.tokens = [])
-        res.send(users)
-    } catch(e) {
-        res.status(500).send(e.message)
-    }
-})
+router.get("/users", auth, async (req, res) => {
+  if (!req.user || req.user.login != "admin") {
+    console.log("you are not admin");
+    return res.status(403).send("Access denied");
+  }
+  try {
+    const users = await User.find({})
+      .populate("articles")
+      .populate({
+        path: "changes",
+        options: {
+          limit: parseInt(req.query.changesLimit),
+        },
+      });
+    users.forEach((user) => (user.tokens = []));
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
 
 //Logout
-router.post('/users/logoutAll', auth, async (req, res) => {
-    try{
-        req.user.tokens = []
-        await req.user.save()
-        res.status(200).send('Successfully logged out from all devices')
-    } catch(e) {
-        res.status(500).send('Logout from all devices failed')
-    }
-})
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.status(200).send("Successfully logged out from all devices");
+  } catch (e) {
+    res.status(500).send("Logout from all devices failed");
+  }
+});
 
 //Get user info
-router.get('/users/me', auth, async (req, res) => {
-    if (!req.user) return res.status(404).send('User is not found')
-    try{
-        let user = req.user;
-        await user.populate('articles').
-        populate({
-            path: 'changes',
-            options: {
-                limit: parseInt(req.query.changesLimit)
-            }
-        }).execPopulate()
-        user.tokens = []
-        res.send(user)
-    } catch(e) {
-        res.status(500).send(e.message)
-    }
-    
-    
-})
+router.get("/users/me", auth, async (req, res) => {
+  if (!req.user) return res.status(404).send("User is not found");
+  try {
+    let user = req.user;
+    await user
+      .populate("articles")
+      .populate({
+        path: "changes",
+        options: {
+          limit: parseInt(req.query.changesLimit),
+        },
+      })
+      .execPopulate();
+    user.tokens = [];
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
 
 module.exports = router;
